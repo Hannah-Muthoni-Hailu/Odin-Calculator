2419e78db7e9bcb6dd6bcba356581928c8154d25
Hannah Hailu Muthoni <muthonihannahhailu@gmail.com>
2025-03-31 07:52:00 +0000

    Display grid for numbers and operators as well as display panel created

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
 let a, b, res;
 
-const parentDiv = document.querySelector(".container");
-
+// Create grid for the numbers and the plus sign
+const parentDiv = document.querySelector("#nums");
+let counter = 1;
 for(var i = 0; i < 4; i++) {
     const childDiv = document.createElement("div");
-    for (var j = 0; j < 4; j++) {
-        const div = document.createElement("div");
-        div.innerHTML = "<p>num</p>";
-        childDiv.appendChild(div);
+    childDiv.style.height = "90px";
+    for (var j = 0; j < 3; j++) {
+        const button = document.createElement("button");
+        button.style.width = "90px";
+        button.style.height = "100%";
+        if (i == 3 && j != 1) {
+            if (j == 2) {
+                button.innerHTML = "<p>clear</p>";
+            } else {
+                button.innerHTML = "<p><br></p>";
+            }
+        } else {
+            button.innerHTML = `${counter}`;
+            if (counter < 9){
+                counter += 1;
+            } else {
+                counter = 0
+            }
+            button.addEventListener("click", () => {
+                console.log(counter);
+            })
+        }
+        childDiv.appendChild(button);
     }
     parentDiv.appendChild(childDiv);
 }
+
+// Create grid for the operators
+const opsDiv = document.querySelector("#ops");
+let ops = ["+", "-", "*", "/", "="]
+ops.forEach(element => {
+    const button = document.createElement("button");
+    button.innerHTML = `${element}`;
+    button.style.width = "100%";
+    button.style.height = "90px";
+    opsDiv.appendChild(button)
+})
+
 
 function add(a, b) {
     return a + b;
